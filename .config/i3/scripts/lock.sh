999e7a3aefda49e577fcbf5c60d4f717089ac4b5
Brett Swift <brettswift@gmail.com>
2019-07-06 09:14:07 -0600

    Add better lock screen

---
+++ .config/i3/scripts/lock.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+icon="$HOME/.config/i3/scripts/icon.png"
+tmpbg='/tmp/screen.png'
+
+(( $# )) && { icon=$1; }
+
+# pause when locking
+spotifycli --status && spotifycli --pause
+
+scrot "$tmpbg"
+convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
+convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
+i3lock -f -e -i "$tmpbg"
