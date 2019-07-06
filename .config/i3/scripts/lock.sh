c8017fa55d817c167c630559edd2571952ecb9ee
Brett Swift <brettswift@gmail.com>
2019-07-06 09:54:17 -0600

    Lock screen after 15 min

--- .config/i3/scripts/lock.sh
+++ .config/i3/scripts/lock.sh
@@ -12,3 +12,8 @@ scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
 i3lock -f -e -i "$tmpbg"
+
+# Turn the screen off after a delay.
+sleep 60
+pgrep i3lock && xset dpms force off
+
